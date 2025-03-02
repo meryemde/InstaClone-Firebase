43d4f96edfa0a77edbde866bbdd1c3b5161084dc
Meryem Demir <meryemdmirr0@gmail.com>
2025-03-02 11:15:41 +0300

    kayıt işlemleri yapıldı

--- FirebaseInstaClone/SettingsViewController.swift
+++ FirebaseInstaClone/SettingsViewController.swift
@@ -6,17 +6,28 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBAction func logoutClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toViewController", sender: nil)
+        do {
+            
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+            
+        } catch {
+            
+            print("error")
+            
+        }
         
     }
     
--- FirebaseInstaClone/ViewController.swift
+++ FirebaseInstaClone/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -14,13 +16,62 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
     @IBAction func sıgnInClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+        if emailText.text != "" && passwordText.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                    
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+                
+                
+            }
+            
+            
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "email/password?")
+        }
+        
+        
+        
+        
     }
     
     @IBAction func sıgnUpClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+                
+            }
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "email/password?")
+        }
+        
+        
+        
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
